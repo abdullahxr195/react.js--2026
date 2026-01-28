bfb3b8fe9df9eefa1bdbffa32449b394d171f469
abdullahxr65-ops <abdullahxr195@gmail.com>
2026-01-28 12:18:30 +0300

    edit react 3

--- store/src/component/home.js
+++ store/src/component/home.js
@@ -1,8 +1,27 @@
+import { useState } from "react"
+
+
+
 export default function Home() {
+
+const[show, setShow] = useState(false);
+
+const handleClick = () => {
+    setShow(!show);
+}
+
     return (
 
         <div id='home-page'>
-            <p>Welcome to the Home Page</p>
+            <button onClick={handleClick}> {show ? 'Hide' : 'Show'} Welcome Message </button>
+
+        {
+
+                show && <img width={200} src="https://i.pinimg.com/736x/1d/a2/cf/1da2cf9466f40336862a5b553524b5e6.jpg" alt="Welcome Banner" />
+
+        }
+
+
         </div>
 
 
