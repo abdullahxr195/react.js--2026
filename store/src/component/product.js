6b7b4f50744533441539edf2fccca2752e0351cb
abdullahxr65-ops <abdullahxr195@gmail.com>
2026-01-28 06:39:21 +0300

    edit react 2

--- store/src/component/product.js
+++ store/src/component/product.js
@@ -1,8 +1,21 @@
+import{ useState } from 'react';
+
 export default function Products() {
+
+   const[count, setCount] = useState(0);
+
+
     return (
 
         <div id='product-page'>
-            <p>Welcome to the Product Page</p>
+            
+            <p>{count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+
+
+
+
         </div>
 
 
